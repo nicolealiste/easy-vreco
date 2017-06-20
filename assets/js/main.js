926328e7e3a1f440af296d0531d8fe8356fef965
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-06-20 02:15:55 -0400

    Se realiza funcion ruta_sin exito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+var map = null;
+var directionsDisplay = null;
+var directionsService = null;
+
 function initMap(){
 	var map = new google.maps.Map(document.getElementById("map"),{
 		zoom: 5,
@@ -7,7 +11,6 @@ function initMap(){
 		streetViewControl: false
 	});
 
-	
 	function buscar(){
 		if(navigator.geolocation){
 			navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
@@ -43,4 +46,43 @@ function initMap(){
 	var autocomplete = new google.maps.places.Autocomplete(inputR);
         autocomplete.bindTo('bounds', map);
 
-};
+    /*trazar ruta*/
+    var directionsDisplay = new google.maps.DirectionsRenderer();/*nos dará las herramientas necesarias para que visualicemos en el navegador toda la información obtenida*/
+    var directionsService = new google.maps.DirectionsService();/*es el servicio que buscará en las bases de datos de Google Maps tanto los puntos como la(s) rutas existentes*/
+ };   
+
+function getDirections(){
+	var start = $('#origen').val();
+	var end = $('#destino').val();
+	if(!start || !end){
+		alert("Start and End addresses are required");
+		return;
+	}
+		//Declaramos una variable con las opciones con las que se va a solicitar la información al objeto DirectionsServices
+	var request = {	/*lo siguiente son parámetros para hacer la búsqueda de acuerdo a los datos introducidos*/
+		 //A origin se le asigna el valor de  la caja de texto origen estableciendo el punto de partida de la ruta.
+	    origin: start,
+	 	 //A destination se le asigna el valor de la caja de texto destino con el que se especifica el punto final de la ruta.
+	    destination: end,
+	}
+	//hace la solicitud a la API con los datos que previamente establecimos (request), creamos una una función interna, que recibe los parámetros de respuesta (response) que es donde vendrían contenidos los datos y un estatus (status) que indica si fue exitosa la solicitud
+	directionsService.route(request, function(response, status) {
+		//Si el estatus es OK procedemos, si no, mandamos un mensaje de error diciendo que no se pudo encontrar ruta entre ambos puntos	
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setMap(map);
+            directionsDisplay.setPanel($("#panel_ruta").get(0));
+		    //hace la magia, imprime en el mapa la ruta y escribe las indicaciones de acuerdo a las 2 instrucciones anteriores.
+            directionsDisplay.setDirections(response);
+        } else {
+            alert("There is no directions available between these two points");
+        }
+    });
+}
+$('#ruta').on('click', function(){ getDirections(); });
+
+	
+	
+	
+
+
+	
